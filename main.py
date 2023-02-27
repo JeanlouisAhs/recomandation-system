28a4496d2577627621702b33f76b5b8bfebbd88b
Jean-Louis <ahouansejeanlouis@gmail.com>
2023-02-27 22:28:57 +0100

    Make clean the code and add console interactive features

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+# TP : Recommandation syste, - collaborative filtering : top n
+__author__ = "Jean-Loui AHOUANSE and Norbert SOUWOUIN"
+__email__ = "rob@spot.colorado.edu"
+__status__ = "Developpement"
+
+# Liens du dépots : https://github.com/JeanlouisAhs/recomandation-system.git
+
 import numpy as np
 
-# Press the green button in the gutter to run the script.
 
 #Création de la matrice
 
@@ -8,32 +14,27 @@ x = int(input("Entrez le nombre de films de la matrice: "))
 y = int(input("Entrez le nombre d'utilisateurs de la matrice: "))
 
 matrix = np.zeros((x, y))
-print("Remplissez la matrice:")
-# for i in range(x):
-#     for j in range(y):
-#         matrix[i][j] = float(input("Entrez la valeur de la case ({}, {}): ".format(i+1, j+1)))
-
-# for i in range(x):
-#     row = input("Entrez la ligne {} séparée par des virgules: ".format(i+1)).split(",")
-#     for j in range(y):
-#         matrix[i][j] = float(row[j])
-
+print("Remplissage la matrice:\n\n")
+print("Vous remplirez la matrice ligne par ligne les éléments sont séparés par des virgules\n")
+print("Si vous ne mettez aucune note entre deux virgule on considère cette valeur comme inexistante \n\n")
 for i in range(x):
-    row = input("Entrez la ligne {} séparée par des virgules: ".format(i+1)).split(",")
+    row = input("Entrez la ligne {} :  ".format(i+1)).split(",")
     for j in range(y):
         if row[j] != '':
             matrix[i][j] = float(row[j])
         else:
             matrix[i][j] = 0.0
 
+print("Voici la matrice initiale : \n")
 print(matrix)
 
 #Demander le n : le top des simularities
 n = int(input("Entrez le Top n "))
 
-# Calcul de la matrice de corrélation
+# Calcul de la matrice de simularité ( ou corrélation)
 corr_matrix = np.corrcoef(matrix)
 
+print("Voici la matrice de simularité : \n")
 print(corr_matrix)
 
 
@@ -42,7 +43,8 @@ for i in range(x):
     for j in range(y):
         if matrix[i][j] == 0:
             similarities = corr_matrix[i]
-            similar_indices = similarities.argsort()[::-1][1:n+1] # Les indices des n films les plus similaires
+            similar_indices = similarities.argsort()[::-1][1:n+1]
+            # Les indices des n films les plus similaires
             numerator = 0
             denominator = 0
             for idx in similar_indices:
@@ -52,4 +54,5 @@ for i in range(x):
             if denominator != 0:
                 matrix[i][j] = numerator / denominator
 
+print("Voici la finale : \n")
 print(matrix)
