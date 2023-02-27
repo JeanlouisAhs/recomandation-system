76f3905cc79ddefd7775f5cb0b2e0b197634a917
Jean-Louis <ahouansejeanlouis@gmail.com>
2023-02-27 22:56:28 +0100

    Fix some errors

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 # TP : Recommandation syste, - collaborative filtering : top n
-__author__ = "Jean-Loui AHOUANSE and Norbert SOUWOUIN"
+__author__ = "Jean-Loui AHOUANSE, Norbert SOUWOUIN ans Osseni ASSAVIE"
 __email__ = "rob@spot.colorado.edu"
 __status__ = "Developpement"
 
 # Liens du dépots : https://github.com/JeanlouisAhs/recomandation-system.git
+# Dernière version sur la branche dev
 
 import numpy as np
 
 
 #Création de la matrice
 
-x = int(input("Entrez le nombre de films de la matrice: "))
-y = int(input("Entrez le nombre d'utilisateurs de la matrice: "))
+x = int(input("Entrez le nombre de films : "))
+y = int(input("Entrez le nombre d'utilisateurs : "))
 
 matrix = np.zeros((x, y))
 print("Remplissage la matrice:\n\n")
 print("Vous remplirez la matrice ligne par ligne les éléments sont séparés par des virgules\n")
-print("Si vous ne mettez aucune note entre deux virgule on considère cette valeur comme inexistante \n\n")
+print("Si vous ne mettez aucune note entre deux virgule on considère cette valeur comme inexistante .\n")
+print("Ces case vides seront remplacé par 0. Ex : ',2,,2,5' correspond à 'O,2,O,2,5'  \n\n")
 for i in range(x):
     row = input("Entrez la ligne {} :  ".format(i+1)).split(",")
     for j in range(y):
@@ -32,25 +34,28 @@ print(matrix)
 n = int(input("Entrez le Top n "))
 
 # Calcul de la matrice de simularité ( ou corrélation)
-corr_matrix = np.corrcoef(matrix)
+similarities_matrix = np.corrcoef(matrix)
 
 print("Voici la matrice de simularité : \n")
-print(corr_matrix)
+print(similarities_matrix)
 
 
 # Remplissage des notes manquantes dans la matrice initiale
 for i in range(x):
     for j in range(y):
         if matrix[i][j] == 0:
-            similarities = corr_matrix[i]
-            similar_indices = similarities.argsort()[::-1][1:n+1]
+            similarities = similarities_matrix[i]
+
             # Les indices des n films les plus similaires
+            similar_indices = similarities.argsort()[::-1][1:n+1]
+
             numerator = 0
             denominator = 0
             for idx in similar_indices:
+                # On vérifie si la note donné pour le film idx par l'utilisateur j est différente de zéro
                 if matrix[idx][j] != 0:
-                    numerator += corr_matrix[i][idx] * matrix[idx][j]
-                    denominator += corr_matrix[i][idx]
+                    numerator += similarities_matrix[i][idx] * matrix[idx][j]
+                    denominator += similarities_matrix[i][idx]
             if denominator != 0:
                 matrix[i][j] = numerator / denominator
 
